23bd4237f769e5578645cfb13605be6dd00034f7
AntoineDeChassey <antoinedechassey@gmail.com>
2018-08-02 17:02:25 +0200

    [Feature] Procfile two process seperation (api / webapp)
    Signed-off-by: AntoineDeChassey <antoinedechassey@gmail.com>

--- webapp/server/server.ts
+++ webapp/server/server.ts
@@ -19,20 +19,21 @@ const forceSSL = () => {
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+//app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/../dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
-
-console.log('Starting webapp...');
 
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log('Started webapp on port: ', process.env.PORT || 8080);
+
